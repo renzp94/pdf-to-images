619c0f3a7fb0b9da620df389af58dd7eaee3ee05
renzp94 <1074720760@qq.com>
2023-03-29 23:58:18 +0800

    chore: 添加logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import PdfToImages from './components/PdfToImages'
 import './app.less'
 import { GithubOutlined } from '@ant-design/icons'
 import { FloatButton } from 'antd'
+import logo from '@/assets/images/logo.png'
 
 const App: React.FC = () => {
   return (
     <div className="page">
       <div className="header">
+        <img className="logo" src={logo} alt="logo" />
         <a href="https://github.com/renzp94/pdf-to-images" target="_blank" rel="noreferrer">
           <GithubOutlined className="header-icon" />
         </a>
