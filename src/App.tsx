348bb6150a6ca63a2311800119ad04a9a2c4c752
renzp94 <1074720760@qq.com>
2023-03-29 23:31:43 +0800

    feat: 添加回到顶部及github链接

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import PdfToImages from './components/PdfToImages'
 import './app.less'
+import { GithubOutlined } from '@ant-design/icons'
+import { FloatButton } from 'antd'
 
 const App: React.FC = () => {
   return (
     <div className="page">
+      <div className="header">
+        <a href="https://github.com/renzp94/pdf-to-images" target="_blank" rel="noreferrer">
+          <GithubOutlined className="header-icon" />
+        </a>
+      </div>
       <PdfToImages />
+      <FloatButton.BackTop type="primary" />
     </div>
   )
 }
